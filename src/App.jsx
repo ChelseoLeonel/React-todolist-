9d554bd1943a1f0b147019580f5f5a1cb7a140a3
Chelseo Miguel <leonelmiguel1612@gmail.com>
2024-05-24 17:01:44 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,21 @@ import { RiCheckboxFill } from 'react-icons/ri';
 import { RiDeleteBin6Line } from 'react-icons/ri';
 import { TbPencilPlus } from 'react-icons/tb';
 
+const toDoListDefault = [
+  {
+    id: "1",
+    text: "Testing 1",
+    complete: false
+  },
+  {
+    id: "2",
+    text: "Testing 2",
+    complete: false
+  }
+]
 
 function App() {
+  const [toDoList, setToDoList] = useState(toDoListDefault);
 
   return (
     <>
@@ -21,6 +34,24 @@ function App() {
             </p>
           </div>
         </div>
+      </div>
+
+      <div className='to-do-list'>
+        {toDoList.map(listItem => {
+          return (
+            <div className="to-do-container">
+              <p className="to-do-checkbox">
+                <RiCheckboxBlankFill />
+              </p>
+              <p className="to-do-text">
+                {listItem.text}
+              </p>
+              <p className="to-do-delete">
+                <RiDeleteBin6Line />
+              </p>
+            </div>
+          )
+        })}
       </div>
     </>
   )
