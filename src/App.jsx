7bc4d34201b049521ba07a833e95e9262990487d
Chelseo Miguel <leonelmiguel1612@gmail.com>
2024-05-24 11:40:19 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './App.css';
+import { RiCheckboxBlankFill } from 'react-icons/ri';
+import { RiCheckboxFill } from 'react-icons/ri';
+import { RiDeleteBin6Line } from 'react-icons/ri';
+import { TbPencilPlus } from 'react-icons/tb';
+
+
+function App() {
+
+  return (
+    <>
+      <div className="header--container">
+        <div className="header">
+          <p className="header--title">
+            My Tasks
+          </p>
+          <div className="header-add-task">
+            <p className="header-add-task-text">
+              <TbPencilPlus />
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
